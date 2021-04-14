784c55d3fada0e478abdb4cf60cfabb8d1035650
Hasar Ali <hasar.ali@protonmail.com>
2021-04-14 00:25:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const ul = document.querySelector('ul');
+  const todoForm = document.querySelector('form');
+  
+  todoForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const li = document.createElement('li');
+    li.innerText = `${formData.get('task')}\n${formData.get('due')}`;
+    ul.appendChild(li);
+  });
 });
